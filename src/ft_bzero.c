df3882bb94f974e727ec70c9112d8fb719171c0f
Vitor Tiago De Magalhaes Marques <vde-maga@c1r14s8.42porto.com>
2025-04-08 15:19:30 +0100

    calma calabreso

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vde-maga <vde-maga@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/07 17:42:50 by vde-maga          #+#    #+#             */
-/*   Updated: 2025/04/08 14:46:53 by vde-maga         ###   ########.fr       */
+/*   Created: 2025/04/07 18:46:49 by vde-maga          #+#    #+#             */
+/*   Updated: 2025/04/08 15:03:28 by vde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
+#include "libft.h" 
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *str);
-void	*ft_memset(void *ptr, int value, size_t n);
-void	*ft_bzero(void *ptr, size_t n);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-#endif
+void	*ft_bzero(void *ptr, size_t n)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	str = ptr;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = 0;
+		i++;
+	}
+	return (str);
+}
--- src/ft_memset.c
+++ src/ft_memset.c
@@ -6,21 +6,23 @@
 /*   By: vde-maga <vde-maga@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 18:15:19 by vde-maga          #+#    #+#             */
-/*   Updated: 2025/04/07 18:35:03 by vde-maga         ###   ########.fr       */
+/*   Updated: 2025/04/08 14:23:16 by vde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h" 
 
-void	*ft_memset(void *str, int c, size_t n)
+void	*ft_memset(void *ptr, int value, size_t n)
 {
-	unsigned char	*p;
+	unsigned char	*str;
+	size_t			i;
 
-	p = s;
-	n = n - 1;
-	while (n > 0)
+	str = ptr;
+	i = 0;
+	while (i < n)
 	{
-		*p++ = (unsigned char) c;
+		str[i] = value;
+		i++;
 	}
-	return (s);
+	return (str);
 }
