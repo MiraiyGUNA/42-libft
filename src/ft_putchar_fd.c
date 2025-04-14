2bdd2cf4b68b2e29222dca14d29f7a0631ec3d03
Miraiy <66632625+MiraiyGUNA@users.noreply.github.com>
2025-04-14 21:25:44 +0100

    Update ft_putchar_fd.c

--- src/ft_putchar_fd.c
+++ src/ft_putchar_fd.c
@@ -11,8 +11,34 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <fcntl.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+/*
+int main(void)
+{
+	int	fd;
+	char	c;
+
+    // Testando saída para o console (file descriptor padrão: 1)
+    c = 'A';
+    ft_putchar_fd(c, 1);
+    ft_putchar_fd('\n', 1);
+
+    // Testando saída para um arquivo
+    fd = open("test_output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1)
+    {
+        write(2, "Erro ao abrir o arquivo.\n", 26);
+        return (1);
+    }
+    ft_putchar_fd('B', fd);
+    ft_putchar_fd('\n', fd);
+    close(fd);
+
+    return (0);
+}
+*/
